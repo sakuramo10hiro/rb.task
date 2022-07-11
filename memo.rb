7f69d0257b9d57c94893f7f7b38b2c88d8d94de4
櫻本 <sakuramotohiroshi@sakuramotonoMacBook-Air.local>
2022-07-11 14:50:09 +0900

    ご指摘箇所の修正

--- memo.rb
+++ memo.rb
@@ -8,27 +8,34 @@ input_type = gets.to_i
 
 if input_type == 1
  puts "拡張子を除いたファイル名を入力して下さい。"
- file_name = gets.chomp
+ file_name = gets.chomp.to_s
  #文字列の末尾から改行コードを取り除いた文字列を返す
  puts "メモの内容を記入して下さい。"
  puts "入力が完了したらCtrl + Dで保存します。"
  memo = STDIN.read
  #複数行を文字列として取得
- CSV.open("#{file_name}.csv","w") do |csv|
+ CSV.open("#{file_name}.csv","w") do |file_name|
  #csvファイルの書き込み、読み込み用のメソッド(wの部分で書き込みの命令)
  #(aだった場合は追記モードで既存データに書き込みがされる↓)
- csv << [memo]
+ file_name << [memo]
  #memoの内容をcsvに書き込み 
   end
 
  elsif input_type == 2
-  puts "メモを編集する"
-  file_name = gets.chomp
+  puts "編集したいファイルを選んで下さい。"
+  updatefile_name = gets.chomp.to_s
 
+  CSV.open("#{updatefile_name}.csv","r") do |updatefile_name|
+  puts updatefile_name.read
+  end
+  #データの読み込み
+
+  puts "メモを編集する"
   puts "入力が完了したらCtrl + Dで保存します。"
   memo = STDIN.read
-  CSV.open("#{file_name}.csv","a") do |csv|
-  csv << [memo]
+
+  CSV.open("#{updatefile_name}.csv","a") do |updatefile_name|
+  updatefile_name << [memo]
   end
 
  else
